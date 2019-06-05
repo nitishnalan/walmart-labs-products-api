c77c8ac849b7462e37be7feb71f388c2c517ee10
nitishnalan <nxl170130@gmail.com>
2019-06-05 11:20:55 -0500

    SpringBootApplication start-up updated

--- src/main/java/org/walmart/restapi/RestApiApplication.java
+++ src/main/java/org/walmart/restapi/RestApiApplication.java
@@ -2,11 +2,18 @@ package org.walmart.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(value = {"org.walmart.services","org.walmart.repository", "org.walmart.models"})
+@EnableJpaRepositories("org.walmart.repository")
+@EntityScan("org.walmart.models")
 public class RestApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RestApiApplication.class, args);
     }
 
