c923bf74f06623ee8daa42f94ab77fd89c903ab9
nitishnalan <nxl170130@gmail.com>
2019-06-05 11:06:00 -0500

    Created Product model and repository

---
+++ src/main/java/org/walmart/models/Product.java
@@ -0,0 +1,111 @@
+package org.walmart.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products_master")
+public class Product {
+
+    public Product(){};
+
+    @Id
+    private String productId;
+    private String productName;
+    private String shortDescription;
+    private String longDescription;
+
+    // TODO: 6/4/2019 check the conversion of the price
+    private String price;
+    private String productImage;
+
+    private float reviewRating;
+    private int reviewCount;
+    private boolean inStock;
+
+    public Product(String productId, String productName, String shortDescription, String longDescription, String price, String productImage, float reviewRating, int reviewCount, boolean inStock) {
+        this.productId = productId;
+        this.productName = productName;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+        this.price = price;
+        this.productImage = productImage;
+        this.reviewRating = reviewRating;
+        this.reviewCount = reviewCount;
+        this.inStock = inStock;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public float getReviewRating() {
+        return reviewRating;
+    }
+
+    public void setReviewRating(float reviewRating) {
+        this.reviewRating = reviewRating;
+    }
+
+    public int getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(int reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+}
+
---
+++ src/main/java/org/walmart/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package org.walmart.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.walmart.models.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+}
