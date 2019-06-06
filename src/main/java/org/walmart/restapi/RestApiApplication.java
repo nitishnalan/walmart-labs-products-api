6782349f19185603e6c2138606854004a0eeb7db
nitishnalan <nxl170130@gmail.com>
2019-06-06 11:56:22 -0500

    Added Bean for Rest Template in SpringBootApplication class

--- src/main/java/org/walmart/restapi/RestApiApplication.java
+++ src/main/java/org/walmart/restapi/RestApiApplication.java
@@ -3,8 +3,10 @@ package org.walmart.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(value = {"org.walmart.services","org.walmart.repository", "org.walmart.models"})
@@ -15,6 +17,11 @@ public class RestApiApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(RestApiApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
