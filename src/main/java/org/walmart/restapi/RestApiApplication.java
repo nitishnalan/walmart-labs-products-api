bf546b62fe4683947983bc96c228be7c5e6bd5db
nitishnalan <nxl170130@gmail.com>
2019-06-17 15:09:17 -0500

    Adding package for helpers in Component Scan

--- src/main/java/org/walmart/restapi/RestApiApplication.java
+++ src/main/java/org/walmart/restapi/RestApiApplication.java
@@ -8,16 +8,14 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@ComponentScan(value = {"org.walmart.services","org.walmart.repository", "org.walmart.models","org.walmart.controllers"})
+@ComponentScan(value = {"org.walmart.services","org.walmart.repository", "org.walmart.models","org.walmart.controllers","org.walmart.helpers"})
 @EnableJpaRepositories("org.walmart.repository")
 @EntityScan("org.walmart.models")
-@Configuration
 @EnableCaching
 @EnableScheduling
 
